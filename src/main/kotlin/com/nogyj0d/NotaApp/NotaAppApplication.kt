d1d987da9ace9c5504a6f6875323e6a0795475f6
Valentin Giarra <valentingiarra2@gmail.com>
2023-07-19 23:50:27 -0300

    first

---
+++ src/main/kotlin/com/nogyj0d/NotaApp/NotaAppApplication.kt
@@ -0,0 +1,11 @@
+package com.nogyj0d.NotaApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotaAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotaAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/nogyj0d/NotaApp/NotaAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nogyj0d.NotaApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotaAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
