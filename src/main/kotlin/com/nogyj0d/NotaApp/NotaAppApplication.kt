9510bb5359f96d63b891acbbb16fd4494e641e0f
Valentin Giarra <valentingiarra2@gmail.com>
2023-07-20 00:09:11 -0300

    dotenv added

--- src/main/kotlin/com/nogyj0d/NotaApp/NotaAppApplication.kt
+++ src/main/kotlin/com/nogyj0d/NotaApp/NotaAppApplication.kt
@@ -1,5 +1,6 @@
 package com.nogyj0d.NotaApp
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,14 @@ import org.springframework.boot.runApplication
 class NotaAppApplication
 
 fun main(args: Array<String>) {
+	val dotenv: Dotenv = Dotenv.load()
+
+	System.setProperty("DB_URL", dotenv["DB_URL"])
+	System.setProperty("DB_USERNAME", dotenv["DB_USERNAME"])
+	System.setProperty("DB_PASSWORD", dotenv["DB_PASSWORD"])
+	System.setProperty("DB_NAME", dotenv["DB_NAME"])
+
 	runApplication<NotaAppApplication>(*args)
+
+	println("---- RUNNING ----")
 }
